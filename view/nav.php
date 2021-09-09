c9223792c5a17386a63c144321da9c6a6ef2d623
aviaanwar <avia.anwar.anika154@gmail.com>
2021-09-10 01:20:46 +0600

    notification icon fixed

--- view/nav.php
+++ view/nav.php
@@ -28,6 +28,7 @@
 
                     <a class="nav-link dropdown-toggle" style="margin-top:10px;" href="#" id="navbarDropdownMenuLink"
                         role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <li><a class="dropdown-item" href='./logout.php'>logout</a></li>
                             <li><a class="dropdown-item" href='./changepw.php'>Change Password</a></li>
@@ -35,10 +36,16 @@
                         </ul>
                 </li>
                 <li class="nav-item dropdown">
+              
 
-                    <a class="nav-link dropdown-toggle" style="margin-top:10px;" href="#" id="navbarDropdownMenuLink"
-                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="nav-link dropdown" style="margin-top:10px;" href="#" id="ownMenuLink"
+                       data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+                        </svg>
+                        </a>
+
+                        <ul class="dropdown-menu" >
                             <li><a class="dropdown-item" href='#'>
                                     <?php
                             include("conn.php");
@@ -65,7 +72,7 @@
                                 if($id==$md){
                                     echo "There is no new post in this catagory";
                                 }
-                                else echo $id." "."<font color =#A59793 >Posted  </font>"."<br>".$desc;
+                                else echo $id." "."<font color =#A59793 >Posted this </font>"."<br> '".$desc."<font color =#A59793 >' <br>in the catagory where you have recently posted</font>";
 
                                 }
                                 else{
